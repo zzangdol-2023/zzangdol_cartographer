704e1f23c79d31fdf3cd55ba01479f465be2ba60
Seunmul <geonhab504@gmail.com>
2023-07-01 05:42:59 +0900

    set commit

--- configuration_files/zzangdol_localization.lua
+++ configuration_files/zzangdol_localization.lua
@@ -6,6 +6,6 @@ include "zzangdol_mapping.lua"
 TRAJECTORY_BUILDER.pure_localization_trimmer = {
   max_submaps_to_keep = 2,
 }
-POSE_GRAPH.optimize_every_n_nodes = 1
+POSE_GRAPH.optimize_every_n_nodes = 3
 
 return options
