6768f13316eb8e697792ed668eb7ee738a563a4f
GeoChoi <snowgeo@gmail.com>
2023-07-01 00:11:18 +0900

    integrate lua file all in one

--- configuration_files/zzangdol_mapping_original.lua
+++ configuration_files/zzangdol_mapping_original.lua
@@ -1,7 +1,105 @@
 -- backpack_2d.lua 개조 파일
 -- include를 풀어서 한 눈에 보기 쉽게 해놓았다.
 
+MAX_3D_RANGE = 60.
+INTENSITY_THRESHOLD = 40
 
+TRAJECTORY_BUILDER_3D = {
+  min_range = 1.,
+  max_range = MAX_3D_RANGE,
+  num_accumulated_range_data = 1,
+  voxel_filter_size = 0.15,
+
+  high_resolution_adaptive_voxel_filter = {
+    max_length = 2.,
+    min_num_points = 150,
+    max_range = 15.,
+  },
+
+  low_resolution_adaptive_voxel_filter = {
+    max_length = 4.,
+    min_num_points = 200,
+    max_range = MAX_3D_RANGE,
+  },
+
+  use_online_correlative_scan_matching = false,
+  real_time_correlative_scan_matcher = {
+    linear_search_window = 0.15,
+    angular_search_window = math.rad(1.),
+    translation_delta_cost_weight = 1e-1,
+    rotation_delta_cost_weight = 1e-1,
+  },
+
+  ceres_scan_matcher = {
+    occupied_space_weight_0 = 1.,
+    occupied_space_weight_1 = 6.,
+    intensity_cost_function_options_0 = {
+        weight = 0.5,
+        huber_scale = 0.3,
+        intensity_threshold = INTENSITY_THRESHOLD,
+    },
+    translation_weight = 5.,
+    rotation_weight = 4e2,
+    only_optimize_yaw = false,
+    ceres_solver_options = {
+      use_nonmonotonic_steps = false,
+      max_num_iterations = 12,
+      num_threads = 1,
+    },
+  },
+
+  motion_filter = {
+    max_time_seconds = 0.5,
+    max_distance_meters = 0.1,
+    max_angle_radians = 0.004,
+  },
+
+  rotational_histogram_size = 120,
+
+  -- TODO(schwoere,wohe): Remove this constant. This is only kept for ROS.
+  imu_gravity_time_constant = 10.,
+  pose_extrapolator = {
+    use_imu_based = false,
+    constant_velocity = {
+      imu_gravity_time_constant = 10.,
+      pose_queue_duration = 0.001,
+    },
+    -- TODO(wohe): Tune these parameters on the example datasets.
+    imu_based = {
+      pose_queue_duration = 5.,
+      gravity_constant = 9.806,
+      pose_translation_weight = 1.,
+      pose_rotation_weight = 1.,
+      imu_acceleration_weight = 1.,
+      imu_rotation_weight = 1.,
+      odometry_translation_weight = 1.,
+      odometry_rotation_weight = 1.,
+      solver_options = {
+        use_nonmonotonic_steps = false;
+        max_num_iterations = 10;
+        num_threads = 1;
+      },
+    },
+  },
+
+  submaps = {
+    high_resolution = 0.10,
+    high_resolution_max_range = 20.,
+    low_resolution = 0.45,
+    num_range_data = 160,
+    range_data_inserter = {
+      hit_probability = 0.55,
+      miss_probability = 0.49,
+      num_free_space_voxels = 2,
+      intensity_threshold = INTENSITY_THRESHOLD,
+    },
+  },
+
+  -- When setting use_intensities to true, the intensity_cost_function_options_0
+  -- parameter in ceres_scan_matcher has to be set up as well or otherwise
+  -- CeresScanMatcher will CHECK-fail.
+  use_intensities = false,
+}
 
 POSE_GRAPH = {
   optimize_every_n_nodes = 90,
@@ -93,8 +191,110 @@ MAP_BUILDER = {
   collate_by_trajectory = false,
 }
 
-include "my_trajectory_builder_2d_original.lua"
-include "my_trajectory_builder_3d_original.lua"
+
+TRAJECTORY_BUILDER_2D = {
+  use_imu_data = true,
+  min_range = 0.,
+  max_range = 30.,
+  min_z = -0.8,
+  max_z = 2.,
+  missing_data_ray_length = 5.,
+  num_accumulated_range_data = 1,
+  voxel_filter_size = 0.025,
+
+  adaptive_voxel_filter = {
+    max_length = 0.5,
+    min_num_points = 200,
+    max_range = 50.,
+  },
+
+  loop_closure_adaptive_voxel_filter = {
+    max_length = 0.9,
+    min_num_points = 100,
+    max_range = 50.,
+  },
+
+  use_online_correlative_scan_matching = false,
+  real_time_correlative_scan_matcher = {
+    linear_search_window = 0.1,
+    angular_search_window = math.rad(20.),
+    translation_delta_cost_weight = 1e-1,
+    rotation_delta_cost_weight = 1e-1,
+  },
+
+  ceres_scan_matcher = {
+    occupied_space_weight = 1.,
+    translation_weight = 10.,
+    rotation_weight = 40.,
+    ceres_solver_options = {
+      use_nonmonotonic_steps = false,
+      max_num_iterations = 20,
+      num_threads = 1,
+    },
+  },
+
+  motion_filter = {
+    max_time_seconds = 5.,
+    max_distance_meters = 0.2,
+    max_angle_radians = math.rad(1.),
+  },
+
+  -- TODO(schwoere,wohe): Remove this constant. This is only kept for ROS.
+  imu_gravity_time_constant = 10.,
+  pose_extrapolator = {
+    use_imu_based = false,
+    constant_velocity = {
+      imu_gravity_time_constant = 10.,
+      pose_queue_duration = 0.001,
+    },
+    imu_based = {
+      pose_queue_duration = 5.,
+      gravity_constant = 9.806,
+      pose_translation_weight = 1.,
+      pose_rotation_weight = 1.,
+      imu_acceleration_weight = 1.,
+      imu_rotation_weight = 1.,
+      odometry_translation_weight = 1.,
+      odometry_rotation_weight = 1.,
+      solver_options = {
+        use_nonmonotonic_steps = false;
+        max_num_iterations = 10;
+        num_threads = 1;
+      },
+    },
+  },
+
+  submaps = {
+    num_range_data = 90,
+    grid_options_2d = {
+      grid_type = "PROBABILITY_GRID",
+      resolution = 0.05,
+    },
+    range_data_inserter = {
+      range_data_inserter_type = "PROBABILITY_GRID_INSERTER_2D",
+      probability_grid_range_data_inserter = {
+        insert_free_space = true,
+        hit_probability = 0.55,
+        miss_probability = 0.49,
+      },
+      tsdf_range_data_inserter = {
+        truncation_distance = 0.3,
+        maximum_weight = 10.,
+        update_free_space = false,
+        normal_estimation_options = {
+          num_normal_samples = 4,
+          sample_radius = 0.5,
+        },
+        project_sdf_distance_to_scan_normal = true,
+        update_weight_range_exponent = 0,
+        update_weight_angle_scan_normal_to_ray_kernel_bandwidth = 0.5,
+        update_weight_distance_cell_to_hit_kernel_bandwidth = 0.5,
+      },
+    },
+  },
+}
+
+
 
 TRAJECTORY_BUILDER = {
   trajectory_builder_2d = TRAJECTORY_BUILDER_2D,
